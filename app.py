e84ca234e2dfee07f47f2d96f5573720db06ceaa
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-09 14:45:38 +0100

    deleted static images

--- app.py
+++ app.py
@@ -420,7 +420,7 @@ def logout():
     return redirect(url_for('index'))
 
 
-# if __name__ == '__main__':
-#     with app.app_context():
-#         init_db()
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        init_db()
+    app.run(debug=True, host='0.0.0.0', port=5000)
