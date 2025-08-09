ca5c56e1b0984f6e8cf3b60f8fa5aeba1d7af2d0
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-09 14:54:28 +0100

    reconfigured server

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ from psycopg2.extras import RealDictCursor
 from datetime import datetime
 app = Flask(__name__)
 load_dotenv()  
+image_dir = os.path.join('static', 'images')
+os.makedirs(image_dir, exist_ok=True)
+
 app.secret_key = os.getenv("SECRET_KEY")
 
 # config.py or app.py
@@ -420,12 +423,9 @@ def logout():
     return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    # Ensure static/images folder exists
-    image_dir = os.path.join('static', 'images')
-    os.makedirs(image_dir, exist_ok=True)
+# if __name__ == '__main__':
 
-    with app.app_context():
-        init_db()
+#     with app.app_context():
+#         init_db()
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#     app.run(debug=True, host='0.0.0.0', port=5000)
