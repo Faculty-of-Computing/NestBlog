e78f8b9ab1e8fb8cd68c771d016d6779e1d68e66
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-09 14:52:19 +0100

    added folder creation on server startup

--- app.py
+++ app.py
@@ -421,6 +421,11 @@ def logout():
 
 
 if __name__ == '__main__':
+    # Ensure static/images folder exists
+    image_dir = os.path.join('static', 'images')
+    os.makedirs(image_dir, exist_ok=True)
+
     with app.app_context():
         init_db()
+    
     app.run(debug=True, host='0.0.0.0', port=5000)
