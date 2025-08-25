394ac4bb94331cbe4eb72d7831e0d2fc5bec4b10
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-08-25 19:43:02 +0100

    removed database init command

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from datetime import datetime
 app = Flask(__name__)
 load_dotenv()  
 
+
 app.secret_key = os.getenv("SECRET_KEY")
 
 app.config['DB_NAME'] = os.getenv("DB_NAME")
@@ -446,7 +447,5 @@ def logout():
 
 if __name__ == '__main__':
 
-    with app.app_context():
-        init_db()
-    
+
     app.run(debug=True, host='0.0.0.0', port=5000)
